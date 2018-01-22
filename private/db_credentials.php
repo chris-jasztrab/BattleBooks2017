6a18a16d047c438230b135085fb8a887c58662a4
chris-jasztrab <chris.jasztrab@gmail.com>
2018-01-22 12:43:46 -0500

    Major change - all tables relational Other changes - Can search questions Also can print in print as a word doc

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -2,7 +2,7 @@
 
 define("DB_SERVER", "msrfc01.corp.mpl.on.ca");
 define("DB_USER", "bob");
-define("DB_PASS", "MPL@dm1n");
-define("DB_NAME", "bob");
+define("DB_PASS", "bob12345");
+define("DB_NAME", "bob_v2");
 
 ?>
--- private/shared/public_header.php
+++ private/shared/public_header.php
@@ -5,7 +5,7 @@
     <title>Battle of the Books <?php if(isset($page_title)) { echo '- ' . h($page_title); } ?></title>
     <meta charset="utf-8">
     <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/public.css'); ?>" />
-    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   </head>
 
   <body>
@@ -14,7 +14,7 @@
 
       <h1>
         <a href="<?php echo url_for('/index.php'); ?>">
-          <img src="<?php echo url_for('/images/gbi_logo.png'); ?>" width="298" height="71" alt="" />
+          <img src="<?php echo url_for('/images/BOB_1.png'); ?>" width="200" height="85" alt="" />
         </a>
       </h1>
     </header>
